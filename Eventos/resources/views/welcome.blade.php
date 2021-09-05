2fd220865d79d45c0459cd0acffd0bc3bb4d7b82
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-09-04 21:31:07 -0300

    atualizano index

--- Eventos/resources/views/welcome.blade.php
+++ Eventos/resources/views/welcome.blade.php
@@ -1,6 +1,29 @@
 @extends('templates.main')
 
 @section('content')
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        
+        <div class="card col-md-3">
+            <img src="/img/event_placeholder.jpg" alt="">
+            <div class="card-body">
+                <p class="card-date">10/09/2020</p>
+                <h5 class="card-title">titulo</h5>
+                <p class="card-participants">X Participantes</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        
+    </div>
+</div>
 
-    
 @endsection
+    
