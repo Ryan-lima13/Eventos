77f2db62d3a1caaa57e6d0e93468628cfdb0dd42
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-09-04 21:42:12 -0300

    criando pagina criar eventos

--- Eventos/routes/web.php
+++ Eventos/routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/',[EventController::class,'index']);
+Route::get('/create',[EventController::class,'create']);
